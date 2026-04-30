9946254c8ddb3718034b25c7b9f2d6e71ea19228
RamdaniRamdane <ramdaniramdane123+1@gmail.com>
2026-04-30 16:52:33 +0200

    disable butons to manage images when there is no image in stack

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import tkinter as tk
 from tkinter import filedialog
 
 import tifffile
@@ -24,45 +25,65 @@ class SegViewApp:
 
     def bind_events(self):
         self.ui.btn.config(command=self.open_dir)
-        self.ui.refuse_but.config(command=lambda: self.save(False))
-        self.ui.validate_but.config(command=lambda: self.save(True))
-        self.ui.zoom_slider.config(command=self.change_z)
-        self.ui.next_btn.config(command=lambda: self.navigate("NEXT"))
-        self.ui.prev_btn.config(command=lambda: self.navigate("PREV"))
+        self.ui.refuse_but.config(state=tk.DISABLED, command=lambda: self.save(False))
+        self.ui.validate_but.config(state=tk.DISABLED, command=lambda: self.save(True))
+        self.ui.zoom_slider.config(state=tk.DISABLED, command=self.change_z)
+        self.ui.next_btn.config(
+            state=tk.DISABLED, command=lambda: self.navigate("NEXT")
+        )
+        self.ui.prev_btn.config(
+            state=tk.DISABLED, command=lambda: self.navigate("PREV")
+        )
 
     def open_dir(self):
         path_dir = filedialog.askdirectory()
-        self.path_dir = path_dir
-        print(path_dir)
-        files = os.listdir(path_dir)
-        self.files = files
-        path_first = path_dir + "/" + files[0]
-        self.open_file(path_first)
+        if os.path.isdir(path_dir):
+            self.path_dir = path_dir
+            files = os.listdir(path_dir)
+            self.files = files
+            path_first = path_dir + "/" + files[0]
+            self.ui.next_btn.config(state=tk.NORMAL)
+            self.ui.prev_btn.config(state=tk.NORMAL)
+            self.ui.zoom_slider.config(state=tk.NORMAL)
+            self.ui.refuse_but.config(state=tk.NORMAL)
+            self.ui.validate_but.config(state=tk.NORMAL)
+
+            self.open_file(path_first)
+
+        else:
+            tk.messagebox.showerror(title="Not found", message="directory not found")
 
     def open_file(self, path):
-        self.file_path = path
+        if os.path.isfile(path):
+            self.file_path = path
 
-        display_path = path if len(path) <= 72 else "..." + path[-70:]
-        self.ui.path_label.config(text=display_path, fg="white")
+            display_path = path if len(path) <= 72 else "..." + path[-70:]
+            self.ui.path_label.config(text=display_path, fg="white")
 
-        self.data = tifffile.imread(path)
-        self.shape = self.data.shape
-        # ici j ai mis le zoom au milieu car generalement le model detect dans le milieu (z,x,y)
-        self.zoom = int(self.shape[0] / 2)
+            self.data = tifffile.imread(path)
+            self.shape = self.data.shape
+            # ici j ai mis le zoom au milieu car generalement le model detect dans le milieu (z,x,y)
+            self.zoom = int(self.shape[0] / 2)
 
-        self.ui.info_label.config(text=f"shape={self.shape} dtype={self.data.dtype}")
+            self.ui.info_label.config(
+                text=f"shape={self.shape} dtype={self.data.dtype}"
+            )
 
-        # load prediction
-        self.prediction, self.has_prediction = self.file_manager.get_prediction(path)
+            # load prediction
+            self.prediction, self.has_prediction = self.file_manager.get_prediction(
+                path
+            )
 
-        # slider config
-        if self.data.ndim > 2:
-            self.ui.zoom_slider.config(to=self.shape[0] - 1)
-            self.ui.zoom_slider.set(self.zoom)
+            # slider config
+            if self.data.ndim > 2:
+                self.ui.zoom_slider.config(to=self.shape[0] - 1)
+                self.ui.zoom_slider.set(self.zoom)
+            else:
+                self.ui.zoom_slider.config(to=0)
+
+            self.update_display()
         else:
-            self.ui.zoom_slider.config(to=0)
-
-        self.update_display()
+            tk.messagebox.showerror(title="Not found", message="file not found")
 
     def navigate(self, direction):
         if direction == "NEXT" and len(self.files):
@@ -71,7 +92,6 @@ class SegViewApp:
                 if self.index < (len(self.files) - 1)
                 else ((self.index + 1) % len(self.files))
             )
-            print(self.index)
             self.open_file(self.path_dir + "/" + self.files[self.index])
         elif direction == "PREV" and len(self.files):
             self.index = (
@@ -80,7 +100,6 @@ class SegViewApp:
                 else ((self.index - 1) % len(self.files))
             )
 
-            print(self.index)
             self.open_file(self.path_dir + "/" + self.files[self.index])
         else:
             print("no files")
@@ -88,7 +107,6 @@ class SegViewApp:
     def update_display(self):
         if self.data is None:
             return
-
         img = self.data if self.data.ndim == 2 else self.data[self.zoom]
 
         if self.has_prediction:
