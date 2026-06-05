f1e94f5e4255268a92a629629100638aa4dc32f5
RamdaniRamdane <ramdaniramdane123+1@gmail.com>
2026-06-05 15:53:56 +0200

    remove skip_processing arg on windows os

--- app.py
+++ app.py
@@ -204,7 +204,7 @@ class SegViewApp:
                         log=self.state.path_log,
                         dir_in=self.state.path_dir,
                         dir_out=self.state.path_out,
-                        skip_preprocessing=False,
+                        # why there is no skip processing in windows ? skip_preprocessing=False,
                     )
 
         except Exception as e:
