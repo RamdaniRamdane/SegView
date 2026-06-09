103ae10689222db7bcd5de5b9469bc230cc9929f
RamdaniRamdane <ramdaniramdane123+1@gmail.com>
2026-06-09 12:56:23 +0200

    separate app statte froma app

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 import shutil
 import threading
 import tkinter as tk
-from dataclasses import dataclass, field
 from time import sleep
 from tkinter import filedialog, messagebox
 
@@ -12,42 +11,10 @@ from biom3d.preprocess import auto_config_preprocess
 from biom3d.train import train
 
 import src.ui.theme as theme
+from src.models.app_state import AppState
 from src.services.file_manager import FileManager
 from src.ui.helpers.edit_mode import EditMode
 from src.ui.helpers.ui_utils import UIutils
-
-
-@dataclass
-class AppState:
-    path_log: str = ""
-    file_path: str = ""
-    prediction_path_file: str = ""
-
-    data: object = None
-    prediction: object = None
-
-    shape: tuple | None = None
-
-    zoom: int = 0
-
-    has_prediction: bool = False
-
-    files: list = field(default_factory=list)
-    files_out: list = field(default_factory=list)
-
-    index: int = 0
-
-    path_dir: str = ""
-    path_out: str = ""
-    path_out_valide: str = ""
-    path_out_list: list = field(default_factory=list)
-    predStarted = False
-
-    edit_mode: bool = False
-    edit_tool: str = ""
-
-    edited: int = 0
-    config_path = str = ""
 
 
 class SegViewApp:
---
+++ src/models/app_state.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class AppState:
+    path_log: str = ""
+    file_path: str = ""
+    prediction_path_file: str = ""
+
+    data: object = None
+    prediction: object = None
+
+    shape: tuple | None = None
+
+    zoom: int = 0
+
+    has_prediction: bool = False
+
+    files: list = field(default_factory=list)
+    files_out: list = field(default_factory=list)
+
+    index: int = 0
+
+    path_dir: str = ""
+    path_out: str = ""
+    path_out_valide: str = ""
+    path_out_list: list = field(default_factory=list)
+    predStarted = False
+
+    edit_mode: bool = False
+    edit_tool: str = ""
+
+    edited: int = 0
+    config_path = str = ""
